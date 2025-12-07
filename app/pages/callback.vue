4492963ee4d7e93908327c6eba09cb4037f21acb
7matic <7matic@protonmail.com>
2025-12-07 21:46:34 +0100

    add callback page for OIDC

---
+++ app/pages/callback.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex items-center gap-2">
+    <NuxtLoadingIndicator />
+    <span>Signing you in...</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { $oidc } = useNuxtApp()
+
+onMounted(async () => {
+  await $oidc.handleCallback()
+  await navigateTo('/courses')
+})
+</script>
