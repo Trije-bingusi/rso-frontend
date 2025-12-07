70a2bfefd493ef4534638d3206eefd3a45fc2218
7matic <7matic@protonmail.com>
2025-12-07 21:43:36 +0100

    nuxt.config.ts to include module configurations and runtime settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  modules: [
+    '@nuxt/ui',
+    '@pinia/nuxt'
+  ],
+
+  css: ['~/assets/css/main.css'],
+
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8081',
+
+      kcUrl: process.env.NUXT_PUBLIC_KC_URL || 'http://localhost:8080',
+      kcRealm: process.env.NUXT_PUBLIC_KC_REALM || 'rso',
+      kcClientId: process.env.NUXT_PUBLIC_KC_CLIENT_ID || 'rso-frontend',
+    }
+  },
+
   devtools: { enabled: true }
 })
