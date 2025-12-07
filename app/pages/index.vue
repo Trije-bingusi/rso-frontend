a80ec821fb66792445c4511d2a7977a19fa36c20
7matic <7matic@protonmail.com>
2025-12-07 21:46:19 +0100

    add theme toggle button

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <UButton variant="ghost" @click="toggle" aria-label="Toggle theme">
+    <UIcon :name="isDark ? 'i-heroicons-moon' : 'i-heroicons-sun'" />
+  </UButton>
+</template>
+
+<script setup lang="ts">
+const colorMode = useColorMode()
+const isDark = computed(() => colorMode.value === 'dark')
+const toggle = () => {
+  colorMode.preference = isDark.value ? 'light' : 'dark'
+}
+</script>
