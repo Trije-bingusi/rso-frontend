f1806eb8fc064613249cd77f5e43e00ae1a31dda
7matic <7matic@protonmail.com>
2025-12-08 03:29:09 +0100

    refactor auth middleware

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -6,16 +6,14 @@ export default defineNuxtRouteMiddleware(async (to) => {
   const auth = useAuthStore()
   const { $oidc } = useNuxtApp()
 
-  // Allow public routes
-  const publicPaths = ['/', '/login', '/callback']
+  // Public routes
+  const publicPaths = ['/login', '/callback']
   if (publicPaths.includes(to.path)) return
 
-  // Ensure restore completed
   if (!auth.isReady) {
     await $oidc.restore()
   }
 
-  // If still no token -> go login
   if (!auth.token) {
     return navigateTo('/login')
   }
